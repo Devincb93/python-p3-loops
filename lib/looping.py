f489102ce966fdd0f35bd5c9ccdec29fbb2dced5
Devin <devincb93@gmail.com>
2024-03-03 19:41:10 -0800

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+   i = 11
+   while i > 0:
+    i -= 1
+    print(f'{i}')
+    print ("Happy New Year!")
+    
+
+   
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = []
+    for integer in int_list:
+       squared_integer = integer ** 2
+       squared_integers.append(squared_integer)
+    return squared_integers
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i < 100:
+       i += 1
+       
+       if (i % 3 == 0 and i % 5 == 0):
+          print ("FizzBuzz")
+       elif (i % 5 == 0):
+          print ("Buzz")
+       elif (i % 3 == 0):
+          print("Fizz")
+       else:
+          print(f'{i}')
+       
+   
